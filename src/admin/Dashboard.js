b11d49c0521867c37855644abacbfb02a5257718
luciaduque248 <luciaduque248@gmail.com>
2022-07-12 22:56:47 -0500

    dashboard admin's was modified(s and n was deleted

--- src/admin/Dashboard.js
+++ src/admin/Dashboard.js
@@ -24,10 +24,10 @@ function Admin_Dashboard() {
       </div>
       <div className='group-menu-admin'>
         <div>
-          <Link to="/" className="group-item-admin"><i class="fa-solid fa-bed"></i> HABITACIONESs</Link>
+          <Link to="/" className="group-item-admin"><i class="fa-solid fa-bed"></i> HABITACIONES</Link>
         </div>
         <div className='border-admin'>
-          <Link to="/" className="group-item-admin"><i class="fa-solid fa-circle-plus"></i> CREAR HABITACIÓNn</Link>
+          <Link to="/" className="group-item-admin"><i class="fa-solid fa-circle-plus"></i> CREAR HABITACIÓN</Link>
         </div>
         <div>
           <Link to="/" className="group-item-admin"><i class="fa-solid fa-user-pen"></i> PERFIL</Link>
