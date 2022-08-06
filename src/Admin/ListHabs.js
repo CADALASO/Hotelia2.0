763345e32d4a13bad5fe027b42282e63644e9764
luciaduque248 <luciaduque248@gmail.com>
2022-08-06 11:37:46 -0500

    design of listhabs was modified

--- src/Admin/ListHabs.js
+++ src/Admin/ListHabs.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { api } from '../utils/peticiones';
 import Nevera from '../assets/img/iconos/nevera.png'
+import Habitacion from '../assets/img/slide3-inicio.png'
 import Footer from '../components/Footer/Footer';
 import ModalEdit from '../Admin/Modal'
 
@@ -23,130 +24,98 @@ function ListHabs() {
     }, [modal])
 
 
-    const handleSelect = async(room, selectedStatus) =>{
-        const response = await axios.put(`${api}${room._id}`, {...room, estado: selectedStatus});
+    // const handleSelect = async(room, selectedStatus) =>{
+    //     const response = await axios.put(`${api}${room._id}`, {...room, estado: selectedStatus});
 
-    }
+    // }
     
     return (
         <div>
             <div className='nav-bar-list'>Header</div>
-
+            
             <div className='container-list-habs'>
                 <h1>HABITACIONES</h1>
 
-                <div className='cards-list-habs'>
+                <div className='cards-list-habitaciones'>
                     {
                         habitaciones?.map(habitacion => (
-                            <div className='flex-list-habs' key={habitacion._id}>
-                                <div className="container-list" key={habitacion._id}>
-                                    <div className="image-card-slide">
-                                        <img src={`https://hoteliakuepa.herokuapp.com${habitacion.img}`} className="habs-cards-list" alt="fotos"/>
+                            <div className='list-cards-vertical'>
+                                <div className='info-image-card'>
+                                    <div className='cards-horizontal' key={habitacion._id}>
+                                        <div className='out'>
+                                            <div className='info-list'>
+                                                <h1 >{habitacion.nombrehab}</h1>
+                                                <p >{habitacion.descripcion}</p>
 
-                                    </div>
-
-                                    <div className="button-card-inicio-list">
-                                        
-                                        {/* <div className='disponible'> */}
-                                            {/* <button className="disponible-cards-list"><i className="fa-solid fa-circle"></i>DISPONIBLE</button> */}
-
-                                            {/* <select name="estado" onChange={(e)=>handleSelect(habitacion,e.target.value)} className="disponible-cards-list">
-                                                <option value="DISPONIBLE">DISPONIBLE</option>
-                                                <option value="NO DISPONIBLE">NO DISPONIBLE</option>
-                                                <option value="EN MANTENIMIENTO">EN MANTENIMIENTO</option>
-                                            </select> */}
-                                        {/* </div> */}
-
-                                        <button
-                                            onClick={() => {
-                                                setModal(true)
-                                                setHabitacion(habitacion)
-                                                console.log(habitacion)
-                                            }}
-                                            className="editar-cards-list">EDITAR
-                                        </button>
-                                        
-                                
-                                    </div>
-
-                                    <div className="card-list">
-                                        {/*  ---------- CARD PARTE delantera ---------- */}
-                                        <div className="card__1-list" key={habitacion._id}>
-                                            <div className="content__1-list">
-                                                <div className="cards-list" id='habitacion'>
-                                                    <div className="contenido-list">
-                                                        <h1 className='list-h1'>{habitacion.nombrehab}</h1>
-                                                        <p className='descrip'>{habitacion.descripcion}</p>
-                                                        <h2 className='precio-front-list'>{habitacion.valornoche} COP / NOCHE</h2>
-                                                    </div>
-
+                                                <div className='price-list'>
+                                                    <h1>PRECIO</h1>
+                                                    <p>{habitacion.valornoche} COP / NOCHE</p>
                                                 </div>
                                             </div>
-                                        </div>
 
-
-                                        {/*  ---------- CARD PARTE TRASERA ---------- */}
-                                        <div className="card__2-list">
-                                            <div className="content__2-list">
-
-                                                <div className="line-1-list">
-                                                    <div className='texto-icono-cardback-list'>
+                                            <div className='details'>
+                                                <div className="lines">
+                                                    <div className='each-thing'>
                                                         <i className="fa-solid fa-bed"></i>
                                                         <p>{habitacion.camas} cama(s)</p>
                                                     </div>
-                                                    <div className='texto-icono-cardback-list'>
+                                                    <div className='each-thing'>
                                                         <i className="fa-solid fa-vault"></i>
-                                                        <p>{habitacion.cajasfuertes?"Si":"No"}</p>
+                                                        <p>{habitacion.cajafuerte ? "Si" : "No"}</p>
                                                     </div>
                                                 </div>
 
-                                                <div className="line-2-list">
-                                                    <div className='texto-icono-cardback-list'>
+                                                <div className="lines">
+                                                    <div className='each-thing'>
                                                         <i className="fa-solid fa-tv"></i>
-                                                        <p>{habitacion.tv?"Si":"No"}</p>
+                                                        <p>{habitacion.tv ? "Si" : "No"}</p>
                                                     </div>
-                                                    <div className='texto-icono-cardback-list'>
+                                                    <div className='each-thing'>
                                                         <i className="fa-solid fa-wifi"></i>
-                                                        <p>{habitacion.wifi?"Si":"No"}</p>
+                                                        <p>{habitacion.wifi ? "Si" : "No"}</p>
                                                     </div>
                                                 </div>
 
-                                                <div className="line-3-list">
-                                                    <div className='texto-icono-cardback-list'>
+                                                <div className="lines">
+                                                    <div className='each-thing'>
                                                         <img src={Nevera} alt='nevera' />
-                                                        <p>{habitacion.nevera?"Si":"No"}</p>
+                                                        <p>{habitacion.nevera ? "Si" : "No"}</p>
                                                     </div>
-                                                    <div className='texto-icono-cardback-list'>
+                                                    <div className='each-thing'>
                                                         <i className="fa-solid fa-bath"></i>
-                                                        <p>{habitacion.banio?"Si":"No"}</p>
+                                                        <p>{habitacion.banio ? "Si" : "No"}</p>
                                                     </div>
                                                 </div>
 
-                                                <div className='precio-list'>
-                                                    <p className='titulo-precio'>PRECIO</p>
-                                                    <p>{habitacion.valornoche} COP / NOCHE</p>
-                                                </div>
-
-
+                                                <button
+                                                    onClick={() => {
+                                                        setModal(true)
+                                                        setHabitacion(habitacion)
+                                                        console.log(habitacion)
+                                                    }}
+                                                    className="edit-rooms">EDITAR
+                                                </button>
                                             </div>
-
                                         </div>
+                                    </div>
+
+                                    <div className='image-room'>
+                                        {/* <img src={`https://hoteliakuepa.herokuapp.com${habitacion.img}`} className="img-rooms" alt="fotos"/> */}
+                                        <img src={Habitacion} alt='foto' />
                                     </div>
                                 </div>
 
                             </div>
-
-
-                        ))
+                        )
+                        )
                     }
                 </div>
                 {
                     modal ? <ModalEdit close={setModal} habitacion={habitacion} /> : null
                 }
-
             </div>
 
-            <Footer />
+            <Footer/>
         </div>
     )
 }
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import FormHab from '../Admin/FormHab'
 import ListHabs from '../Admin/ListHabs'
-import Prueba from '../Admin/Prueba'
-
-
 import Inicio from '../pages/Inicio'
 import Login from '../pages/Login'
 
@@ -19,7 +17,7 @@ function AppRoutes() {
             <Route path='/Hotelia/form-habitaciones' element={<FormHab/>} ></Route>
             <Route path='/Hotelia/login' element={<Login/>} ></Route>
 
-            <Route path='/Hotelia/prueba' element={<Prueba/>} ></Route>
+            
         </Routes>
     </BrowserRouter>
   )
